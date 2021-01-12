9b4829c4c2ea9ee31b126b70aeb733b52d87f952
mrehasan <66246521+mrehasan@users.noreply.github.com>
2021-01-12 20:38:41 +0500

    Delete Assignment-3_137746 directory

--- Assignment-3_137746/Task-4/Task4.js
+++
@@ -1,9 +0,0 @@
-document.write("<h1>Cities List :</h2>");
-var cities= [" Karachi ", " Lahore "," Islamabad "," Quetta "," Peshawar "];
-
-document.write(cities+"<br>");
-
-document.write("<h1>Selected Cities :</h2>");
-arr=["Lahore,Islamabad,Quetta"];
-document.write(arr);
-
